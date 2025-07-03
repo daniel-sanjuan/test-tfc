b821ad21c93f128ce300ecd66aa82d1d9b7b3c55
angeldaniel <daniel.sanjuan@sngular.com>
2025-07-03 11:04:54 +0200

    feat: first version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "auto-expire" {
   project                     = "pg-gccoe-daniel-sanjuan"
-  name                        = "auto-expiring-bucket"
+  name                        = "pg-gccoe-daniel-sanjuan-auto-expiring-bucket"
   location                    = "EU"
   force_destroy               = true
   uniform_bucket_level_access = true
