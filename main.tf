d847655ec68894c5792cb41b5896707120770891
angeldaniel <daniel.sanjuan@sngular.com>
2026-03-20 10:30:32 +0100

    feat: test parallelism

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ resource "google_storage_bucket" "auto-expire" {
 }
 
 
-# resource "terraform_data" "parallelism_test" {
-#   count = 40
+resource "terraform_data" "parallelism_test" {
+  count = 40
 
-#   # The 'input' stores the index so each resource is unique
-#   input = count.index
+  # The 'input' stores the index so each resource is unique
+  input = count.index
 
-#   provisioner "local-exec" {
-#     # 'sleep 10' forces the resource to stay in "Creating..." state.
-#     # If parallelism=20 is working, all 20 will start simultaneously.
-#     # If it's the default (10), you'll see two batches of 10.
-#     command = "echo 'Starting resource ${count.index}...'; sleep 20; echo 'Finished resource ${count.index}.'"
-#   }
-# }
+  provisioner "local-exec" {
+    # 'sleep 10' forces the resource to stay in "Creating..." state.
+    # If parallelism=20 is working, all 20 will start simultaneously.
+    # If it's the default (10), you'll see two batches of 10.
+    command = "echo 'Starting resource ${count.index}...'; sleep 20; echo 'Finished resource ${count.index}.'"
+  }
+}
