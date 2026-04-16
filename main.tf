f3ace048a5c796638fade96a36ee49b73ee24de0
angeldaniel <daniel.sanjuan@sngular.com>
2026-04-16 12:59:06 +0200

    feat: test workflow

--- main.tf
+++ main.tf
@@ -5,14 +5,6 @@ resource "google_storage_bucket" "auto-expire" {
   location                    = "EU"
   force_destroy               = true
   uniform_bucket_level_access = true
-
-  #   lifecycle_rule {
-  #     condition {
-  #       age = 3
-  #     }
-  #     action {
-  #       type = "Delete"
-  #     }
-  #   }
 }
+
 
